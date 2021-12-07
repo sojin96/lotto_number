d5db47f7dcc17a1e016a827ddf5a86ba87efd2fd
sojin <aphroditeso@naver.com>
2021-12-07 15:55:05 +0900

    xxxx

--- test2.py
+++ test2.py
@@ -30,7 +30,7 @@ def get_last_lotto_numbers():
             break
 
     print(LAST_LOTTO_NUMBERS)
-    return json.load(json.dumps(LAST_LOTTO_NUMBERS))
+    return json.loads(json.dumps(LAST_LOTTO_NUMBERS))
 
 # 입력받은 예상 번호 문자열을 리스트로 변환
 # http://xxxxxxxx/api/insert_lotto_numbers?numbers=11 22 33 44 32 17
@@ -48,7 +48,7 @@ def insert_lotto_numbers(numbers):
 
     print(200, LOTTO_NUMBERS)
 
-    return json.load(json.dumps(LOTTO_NUMBERS))
+    return json.loads(json.dumps(LOTTO_NUMBERS))
 
 # 자동 번호 가져오기
 
@@ -68,7 +68,7 @@ def get_auto_lotto_numbers():
 
     print(LOTTO_NUMBERS)
 
-    return json.load(json.dumps(LOTTO_NUMBERS))
+    return json.loads(json.dumps(LOTTO_NUMBERS))
 
 # 번호 비교
 
@@ -81,7 +81,7 @@ def check_lotto_numbers():
     if LOTTO_NUMBERS == LAST_LOTTO_NUMBERS:
         result = True
 
-    return json.load(json.dumps({"RESULT": result}))
+    return json.loads(json.dumps({"RESULT": result}))
 
 
 if __name__ == "__main__":
